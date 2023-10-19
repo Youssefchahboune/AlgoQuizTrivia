63013a5eceec3d936a36aa1bf973b0cb1a57578c
test <test>
2023-10-19 16:13:30 -0400

    added the navigation between pages and styled the quiz page

--- AlgoQuizTrivia/ContentView.swift
+++ AlgoQuizTrivia/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct NavBar: View {
+    @Binding var page: String
     @State private var isMenuVisible = false
     @State private var menuHeight: CGFloat = 0.0
 
@@ -32,35 +33,123 @@ struct NavBar: View {
                         .padding(.trailing, 20)
                 }
             }.frame(height: 70).background(Color.blue)
-
-            ZStack(alignment: .leading) {
-                Rectangle()
-                    .frame(height: menuHeight)
-                    .foregroundColor(.blue)
-                    .animation(.easeInOut(duration: 0.3))
-
-                VStack(alignment: .leading, spacing: 10) {
-                    Button("Take Quiz") {
-                        // Add action for Button 1
-                    }
-                    Button("Quizzes Results") {
-                        // Add action for Button 2
+                ZStack(alignment: .leading) {
+                    Rectangle()
+                        .frame(height: menuHeight)
+                        .foregroundColor(.blue)
+                        .animation(.easeInOut(duration: 0.3))
+                    if isMenuVisible {
+                        VStack(alignment: .leading, spacing: 10) {
+                            Button("Quiz") {
+                                page = "quizpage"
+                            }
+                            Button("Results") {
+                                page = "resultpage"
+                            }
+                        }.foregroundColor(.white)
+                            .padding(.leading,20)
+                            .opacity(isMenuVisible ? 1.0 : 0.0)
+                            .disabled(!isMenuVisible)
                     }
                 }
-                .foregroundColor(.white)
-                .padding(.leading,20)
-            }
         }
     }
 }
 
 struct ContentView: View {
     
+    @State var page = "quizpage"
+    
     var body: some View {
         
-        VStack{
-            NavBar().zIndex(1)
+        VStack(alignment: .leading,spacing: 0){
+                NavBar(page : $page)
+                if page == "quizpage"{
+                    QuizPage(page: $page)
+                } else if page == "resultpage" {
+                    ResultsPage()
+                }
+                
         }.frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+    }
+}
+
+struct QuizPage: View {
+    @State private var isTextVisible = false
+    @Binding var page : String
+    var body: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            if isTextVisible {
+                Text("Welcome to AlgoQuiz 👨‍💻")
+                    .fontWeight(.black)
+                    .font(.system(size: 40))
+                    .foregroundColor(.blue)
+                    .padding(.vertical, 20)
+                    .offset(x: 0) // Start from the left edge
+                    .animation(Animation.easeOut(duration: 0.5))
+                
+                Text("AlgoQuiz is your gateway to testing your knowledge across a wide range of technologies and topics.")
+                    .padding(.trailing, 50)
+                    .padding(.vertical)
+                    .offset(x: 0) // Start from the left edge
+                    .animation(Animation.easeOut(duration: 0.5).delay(0.1))
+                
+                Text("Are you ready to challenge yourself and prove your expertise?")
+                    .padding(.trailing, 50)
+                    .padding(.vertical)
+                    .offset(x: 0) // Start from the left edge
+                    .animation(Animation.easeOut(duration: 0.5).delay(0.2))
+                
+                Text("Click the Ready button to embark on your AlgoQuiz journey and demonstrate your tech-savvy prowess or click the show Results to see all results from previous quiz!")
+                    .padding(.trailing, 30)
+                    .padding(.vertical)
+                    .offset(x: 0) // Start from the left edge
+                    .animation(Animation.easeOut(duration: 0.5).delay(0.3))
+                
+                HStack {
+                    Button("Ready") {}.frame(width: 100, height: 40)
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(50)
+                        .fontWeight(.black)
+                        .padding(.trailing, 25)
+                        .padding(.vertical)
+                        .offset(x: 0) // Start from the left edge
+                        .animation(Animation.easeOut(duration: 0.5).delay(0.3))
+                    
+                    
+                    
+                    Button("Show Results") {
+                        page = "resultpage"
+                    }.frame(width: 200, height: 40)
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                        .cornerRadius(50)
+                        .fontWeight(.black)
+                        .padding(.vertical)
+                        .offset(x: 0) // Start from the left edge
+                        .animation(Animation.easeOut(duration: 0.5).delay(0.3))
+                    
+                }
+            }
+        }
+        .padding(.leading, 20)
+        .padding(.top,50)
+        .onAppear {
+            withAnimation {
+                isTextVisible = true
+            }
+        }
+    }
+}
+
+
+struct ResultsPage : View {
+    var body: some View {
+        
+        VStack {
+            Text("Result page").fontWeight(.black).font(.system(size: 40))
+        }.padding(.leading,20)
     }
 }
 
